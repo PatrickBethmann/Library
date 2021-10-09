0eb3e69153623bd1d6ae2fce5668745659b0b2b3
Patrick Bethmann <patrickbethmann@outlook.de>
2021-10-09 15:36:12 +0200

    Added createBookCard() Fcuntion adn improved design

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const bookGrid = document.querySelector("#bookGrid");
 
 function Book(title, author, pages, isRead) {
     this.title = title;
@@ -16,6 +17,8 @@ function Book(title, author, pages, isRead) {
 }
 
 
+
+
 // Test examples
 let book1 = new Book("First book", "Patrick", 24, true);
 let book2 = new Book("Second book", "Tobias", 2042, false);
@@ -23,7 +26,37 @@ myLibrary.push(book1, book2);
 
 console.log("--------------------------------------------------")
 console.log("All books:")
-myLibrary.forEach(element => {
-    console.log(element.info());
+myLibrary.forEach(book => {
+    createBookCard(book);
+    console.log(book.info());
 });
 console.log("--------------------------------------------------")
+
+function createBookCard(book) {
+    // Create BookCard Elements
+    const bookCard = document.createElement("div");
+    const title = document.createElement("h3");
+    const author = document.createElement("h3");
+    const pages = document.createElement("h3");
+    const readBtn = document.createElement("button");
+    const removeBtn = document.createElement("button");
+
+    // Edit Contents
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+
+    // Set Classes
+    bookCard.classList.add("bookCard");
+    readBtn.classList.add("btn");
+    removeBtn.classList.add("btn");
+
+    // append elements
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(readBtn);
+    bookCard.appendChild(removeBtn);
+    bookGrid.appendChild(bookCard);
+
+}
